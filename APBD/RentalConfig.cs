472bbbb80107fabdd1c00b47a5c8cffe211d4439
Kuba Makolski <jakubm2004@vp.pl>
2026-03-20 23:06:15 +0100

    Update RentalService and add Penalty calc.

---
+++ APBD/RentalConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD;
+
+public static class RentalConfig
+{
+    public const decimal PenaltyPerDay = 50m;
+    public const int StudentMaxRentals = 2;
+    public const int EmployeeMaxRentals = 5;
+}

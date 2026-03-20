a10d2af13bbf6be3748c243cd842cc9115cc67a3
Kuba Makolski <jakubm2004@vp.pl>
2026-03-20 23:08:32 +0100

    Update construct in Employee and Student

--- APBD/Models/Users/Student.cs
+++ APBD/Models/Users/Student.cs
@@ -1,11 +1,13 @@
 namespace APBD.Models.Users;
 
+using APBD;
+
 public  class Student : User
 {
     public string Specialization { get; private set; }
     public string DegreeCourse  {get; private set;}
     
-    public Student(string firstName, string lastName, int maxRentals, string specialization, string degreeCourse) : base(firstName, lastName, maxRentals: 2)
+    public Student(string firstName, string lastName, int maxRentals, string specialization, string degreeCourse) : base(firstName, lastName, RentalConfig.StudentMaxRentals)
     {
         Specialization = specialization;
         DegreeCourse = degreeCourse;

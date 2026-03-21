472d25e4cc372d069e0d7b1b2f2f4818c61534a3
Kuba Makolski <jakubm2004@vp.pl>
2026-03-21 14:47:50 +0100

    Finish program

--- APBD/Program.cs
+++ APBD/Program.cs
@@ -1,7 +1,23 @@
-﻿public class Program
+﻿using APBD.Models;
+using APBD.Models.Equipments;
+using APBD.Models.Users;
+using APBD.Services;
+using APBD.UI;
+
+public class Program
 {
-        public static void Main()
-        {
-        Console.WriteLine("Hello, World!");
-        }
+    public static void Main()
+    {
+        List<Rental> rentals = new List<Rental>();
+        List<Equipment> equipment = new List<Equipment>();
+        List<User> users = new List<User>();
+
+        RentalService rentalService = new RentalService(rentals);
+        ReportService reportService = new ReportService(rentals);
+        EquipmentService equipmentService = new EquipmentService(equipment);
+        UserService userService = new UserService(users);
+
+        Menu menu = new Menu(rentalService, reportService, equipmentService, userService);
+        menu.Run();
+    }
 }
